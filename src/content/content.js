240ef9e1566916eca5a9ecdac2b1f32a42a84dee
daniel-budai <danielbudai@tutanota.com>
2024-05-23 09:15:56 +0200

    shift + wheel

--- src/content/content.js
+++ src/content/content.js
@@ -1,7 +1,9 @@
 window.addEventListener("wheel", (event) => {
-  if (event.deltaY < 0) {
-    chrome.runtime.sendMessage({ action: "switchTab", direction: "left" });
-  } else {
-    chrome.runtime.sendMessage({ action: "switchTab", direction: "right" });
+  if (event.shiftKey) {
+    if (event.deltaY < 0) {
+      chrome.runtime.sendMessage({ action: "switchTab", direction: "left" });
+    } else {
+      chrome.runtime.sendMessage({ action: "switchTab", direction: "right" });
+    }
   }
 });
